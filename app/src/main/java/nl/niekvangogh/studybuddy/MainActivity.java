84dcd71cb7579507d04f9763bd80071822d5056e
Sander van Dinteren <sanvandinteren@gmail.com>
2018-12-11 12:07:34 +0100

    Added design elements for MaterialCardview

--- app/src/main/java/nl/niekvangogh/studybuddy/MainActivity.java
+++ app/src/main/java/nl/niekvangogh/studybuddy/MainActivity.java
@@ -8,7 +8,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.dashboard_carditem);
+
     }
 
 }
