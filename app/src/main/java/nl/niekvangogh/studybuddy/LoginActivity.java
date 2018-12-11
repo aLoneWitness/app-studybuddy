fb91c06d5f9c103d5f3219d6a5d54afb1ff657da
Sander van Dinteren <sanvandinteren@gmail.com>
2018-12-11 10:11:27 +0100

    mainActivity added and Button with Intent.

--- app/src/main/java/nl/niekvangogh/studybuddy/LoginActivity.java
+++ app/src/main/java/nl/niekvangogh/studybuddy/LoginActivity.java
@@ -1,6 +1,9 @@
 package nl.niekvangogh.studybuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,13 +11,26 @@ import androidx.appcompat.app.AppCompatActivity;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
+    private Button button;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = (Button) findViewById(R.id.material_loginbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
     }
 
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
 
